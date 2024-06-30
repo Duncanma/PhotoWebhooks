0ba0f8cf3adbcdf71f96d15e81a88e5a23dc47d1
Duncan Mackenzie <duncanma@duncanmackenzie.net>
2024-06-30 13:04:14 -0700

    Moving test project into git

---
+++ PhotoWebhooksTests/FunctionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhotoWebhooks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoWebhooks.Tests
+{
+    [TestClass()]
+    public class FunctionsTests
+    {
+        [TestMethod()]
+        public void SimplifyReferrerTest()
+        {
+
+            Assert.AreEqual<string>(FunctionsHelpers.SimplifyReferrer("https://fred.com"), "fred.com");
+            Assert.AreEqual<string>(FunctionsHelpers.SimplifyReferrer("https://google.com"), "Google");
+            Assert.AreEqual<string>(FunctionsHelpers.SimplifyReferrer("https://dev.to/thispage"), "Dev.To");
+            Assert.AreEqual<string>(FunctionsHelpers.SimplifyReferrer("https://google.ca"), "Google");
+        }
+    }
+}
